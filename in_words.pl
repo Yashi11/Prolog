1b21a90cc5f4ebcc7b57022b4d6ac0fea10ddd7f
Yashi <yashasvi4882live.com>
2021-12-20 18:55:18 +0530

    Added Programs for implementing various list operations on list like deletion,checking membership,translation,concatenation

---
+++ in_words.pl
@@ -0,0 +1,12 @@
+interm(0,zero).
+interm(1,one).
+interm(2,two).
+interm(3,three).
+interm(4,four).
+interm(5,five).
+interm(6,six).
+interm(7,seven).
+interm(8,eight).
+interm(9,nine).
+to_words([X],[Y]):-interm(X,Y).
+to_words([X|X1],[Y|Y1]):-interm(X,Y),to_words(X1,Y1).
