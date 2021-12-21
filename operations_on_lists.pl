449e68d1fad86c63b450d1ef1aab2307cf421cbb
Yashi <yashasvi4882live.com>
2021-12-21 20:58:12 +0530

    Added more list operations and added some modifications

--- operations_on_lists.pl
+++ operations_on_lists.pl
@@ -6,3 +6,11 @@ concatenation([],L,L).
 concatenation([X1|L1],L2,[X1|L3]):-concatenation(L1,L2,L3).
 deletion(X,[X|TAIL],TAIL).
 deletion(X,[HEAD|TAIL],[HEAD|Y]):-deletion(X,TAIL,Y).
+list_permutation([],[]).
+list_permutation(L,[X|P]):-deletion(X,L,L1),list_permutation(L1,P).
+list_append(X,Y,[X|Y]).
+last_element([X],X).
+last_element([X|Y],L):-last_element(Y,L).
+nodoubles([],[]).
+nodoubles([X],[X]).
+nodoubles([X1,X2|TAIL],Y):-deletion(X1,[X2|TAIL],Y2),nodoubles(Y2,Z1),list_append(Y2,Z1,Z).
--- types_of_lines.pl
+++ types_of_lines.pl
@@ -1,7 +1,6 @@
 % Program to identify wheather a line is horizontal,vertical or oblique.
-is_line(seg(point(X1,Y1),point(X2,Y2))):-
-    X1 \== X2 ; Y1 \==Y2.
-vertical(seg(point(X,_),point(X,_))).
+is_line(seg(point(X1,Y1),point(X2,Y2))):- X1 \== X2 ; Y1 \==Y2.
+vertical(seg(point(X,_),point(X,_))):-.
 horizontal(seg(point(_,Y),point(_,Y))).
 oblique(seg(point(X1,Y1),point(X2,Y2))):-
 X1 \== X2.
